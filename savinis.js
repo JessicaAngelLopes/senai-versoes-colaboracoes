b25f82543272b8a406990fb8a7132f7b13b36b53
JessicaAngelLopes <jessicaangel_lopes@hotmail.com>
2022-03-14 12:30:59 -0300

    atualização 14/03

---
+++ savinis.js
@@ -0,0 +1,12 @@
+const alunos = 14
+for (let index = 0; index <= alunos; index ++){
+if (index == 0){
+    console.log ('numero zero');
+}
+else if (index % 2 == 0){
+    console.log ("o numero é par: " + " " + index);
+
+} else {
+    console.log ( "o numero é impar:" + " " + index)
+}
+}
